1007d8f139d7b218e2825a96c080521a2e5d19c3
Becca Petrin <beccapetrin@gmail.com>
2020-01-10 15:32:31 -0800

    working client tests

--- sdk/helper/kubernetes/client_test.go
+++ sdk/helper/kubernetes/client_test.go
@@ -1,1 +1,154 @@
 package kubernetes
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"path"
+	"strings"
+	"testing"
+)
+
+const (
+	testNamespace = "default"
+	testPodname   = "shell-demo"
+)
+
+func TestClient(t *testing.T) {
+	closeFunc := testServer(t)
+	defer closeFunc()
+
+	client, err := NewLightWeightClient()
+	if err != nil {
+		t.Fatal(err)
+	}
+	e := &env{
+		client: client,
+	}
+	e.TestGetPod(t)
+	e.TestGetPodNotFound(t)
+	e.TestUpdatePodTags(t)
+	e.TestUpdatePodTagsNotFound(t)
+}
+
+type env struct {
+	client LightWeightClient
+}
+
+func (e *env) TestGetPod(t *testing.T) {
+	if err := e.client.GetPod(testNamespace, testPodname); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func (e *env) TestGetPodNotFound(t *testing.T) {
+	err := e.client.GetPod(testNamespace, "no-exist")
+	if err == nil {
+		t.Fatal("expected error because pod is unfound")
+	}
+	if err != ErrNotFound {
+		t.Fatalf("expected %q but received %q", ErrNotFound, err)
+	}
+}
+
+func (e *env) TestUpdatePodTags(t *testing.T) {
+	if err := e.client.UpdatePodTags(testNamespace, testPodname, &Tag{
+		Key:   "fizz",
+		Value: "buzz",
+	}); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func (e *env) TestUpdatePodTagsNotFound(t *testing.T) {
+	err := e.client.UpdatePodTags(testNamespace, "no-exist", &Tag{
+		Key:   "fizz",
+		Value: "buzz",
+	})
+	if err == nil {
+		t.Fatal("expected error because pod is unfound")
+	}
+	if err != ErrNotFound {
+		t.Fatalf("expected %q but received %q", ErrNotFound, err)
+	}
+}
+
+func testServer(t *testing.T) (closeFunc func()) {
+	// Edit the url scheme for our test server, and use our
+	// fixtures to supply the token and ca.crt.
+	scheme = "http://"
+	tokenFile = "fixtures/token"
+	rootCAFile = "fixtures/ca.crt"
+
+	// Read in the fixtures we'll use for responses.
+	getPodSuccessResp, err := ioutil.ReadFile("fixtures/get-pod-response.json")
+	if err != nil {
+		t.Fatal(err)
+	}
+	updatePodTagsSuccessResp, err := ioutil.ReadFile("fixtures/update-pod-tags-response.json")
+	if err != nil {
+		t.Fatal(err)
+	}
+	notFoundResp, err := ioutil.ReadFile("fixtures/not-found-response.json")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		namespace, podName, err := parsePath(r.URL.Path)
+		if err != nil {
+			w.WriteHeader(400)
+			w.Write([]byte(fmt.Sprintf("unable to parse %s: %s", r.URL.Path, err.Error())))
+			return
+		}
+
+		switch {
+		case namespace != testNamespace, podName != testPodname:
+			w.WriteHeader(404)
+			w.Write(notFoundResp)
+			return
+		case r.Method == http.MethodGet:
+			w.WriteHeader(200)
+			w.Write(getPodSuccessResp)
+			return
+		case r.Method == http.MethodPatch:
+			w.WriteHeader(200)
+			w.Write(updatePodTagsSuccessResp)
+		default:
+			w.WriteHeader(400)
+			w.Write([]byte(fmt.Sprintf("unexpected request method: %s", r.Method)))
+		}
+	}))
+
+	// ts.URL example: http://127.0.0.1:35681
+	urlFields := strings.Split(ts.URL, "://")
+	if len(urlFields) != 2 {
+		t.Fatal("received unexpected test url: " + ts.URL)
+	}
+	urlFields = strings.Split(urlFields[1], ":")
+	if len(urlFields) != 2 {
+		t.Fatal("received unexpected test url: " + ts.URL)
+	}
+	if err := os.Setenv(EnvVarKubernetesServiceHost, urlFields[0]); err != nil {
+		t.Fatal(err)
+	}
+	if err := os.Setenv(EnvVarKubernetesServicePort, urlFields[1]); err != nil {
+		t.Fatal(err)
+	}
+	return ts.Close
+}
+
+// The path should be formatted like this:
+// fmt.Sprintf("/api/v1/namespaces/%s/pods/%s", namespace, podName)
+func parsePath(urlPath string) (namespace, podName string, err error) {
+	original := urlPath
+	podName = path.Base(urlPath)
+	urlPath = strings.TrimSuffix(urlPath, "/pods/"+podName)
+	namespace = path.Base(urlPath)
+	if original != fmt.Sprintf("/api/v1/namespaces/%s/pods/%s", namespace, podName) {
+		return "", "", fmt.Errorf("received unexpected path: %s", original)
+	}
+	return namespace, podName, nil
+}
--- sdk/helper/kubernetes/config.go
+++ sdk/helper/kubernetes/config.go
@@ -10,24 +10,27 @@ import (
 	"github.com/hashicorp/vault/sdk/helper/certutil"
 )
 
-var ErrNotInCluster = errors.New("unable to load in-cluster configuration, KUBERNETES_SERVICE_HOST and KUBERNETES_SERVICE_PORT must be defined")
+const (
+	EnvVarKubernetesServiceHost = "KUBERNETES_SERVICE_HOST"
+	EnvVarKubernetesServicePort = "KUBERNETES_SERVICE_PORT"
+)
 
-// TODO write a test using this fixture, note no newline at end.
-/*
-root@minikube:/# cat /var/run/secrets/kubernetes.io/serviceaccount/token
-eyJhbGciOiJSUzI1NiIsImtpZCI6IjZVQU91ckJYcTZKRHQtWHpaOExib2EyUlFZQWZObms2d25mY3ZtVm1NNUUifQ.eyJpc3MiOiJrdWJlcm5ldGVzL3NlcnZpY2VhY2NvdW50Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9uYW1lc3BhY2UiOiJkZWZhdWx0Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9zZWNyZXQubmFtZSI6ImRlZmF1bHQtdG9rZW4tNWZqdDkiLCJrdWJlcm5ldGVzLmlvL3NlcnZpY2VhY2NvdW50L3NlcnZpY2UtYWNjb3VudC5uYW1lIjoiZGVmYXVsdCIsImt1YmVybmV0ZXMuaW8vc2VydmljZWFjY291bnQvc2VydmljZS1hY2NvdW50LnVpZCI6ImY0NGUyMDIxLTU2YWItNDEzNC1hMjMxLTBlMDJmNjhmNzJhNiIsInN1YiI6InN5c3RlbTpzZXJ2aWNlYWNjb3VudDpkZWZhdWx0OmRlZmF1bHQifQ.hgMbuT0hlxG04fDvI_Iyxtbwc8M-i3q3K7CqIGC_jYSjVlyezHN_0BeIB3rE0_M2xvbIs6chsWFZVsK_8Pj6ho7VT0x5PWy5n6KsqTBz8LPpjWpsaxpYQos0RzgA3KLnuzZE8Cl-v-PwWQK57jgbS4AdlXujQXdtLXJNwNAKI0pvCASA6UXP55_X845EsJkyT1J-bURSS3Le3g9A4pDoQ_MUv7hqa-p7yQEtFfYCkq1KKrUJZMRjmS4qda1rg-Em-dw9RFvQtPodRYF0DKT7A7qgmLUfIkuky3NnsQtvaUo8ZVtUiwIEfRdqw1oQIY4CSYz-wUl2xZa7n2QQBROE7wroot@minikube:/#
-*/
+var (
+	ErrNotInCluster = errors.New("unable to load in-cluster configuration, KUBERNETES_SERVICE_HOST and KUBERNETES_SERVICE_PORT must be defined")
+
+	// These are presented as variables so they can be updated
+	// to point at test fixtures if needed.
+	scheme     = "https://"
+	tokenFile  = "/var/run/secrets/kubernetes.io/serviceaccount/token"
+	rootCAFile = "/var/run/secrets/kubernetes.io/serviceaccount/ca.crt"
+)
 
 // inClusterConfig returns a config object which uses the service account
 // kubernetes gives to pods. It's intended for clients that expect to be
 // running inside a pod running on kubernetes. It will return ErrNotInCluster
 // if called from a process not running in a kubernetes environment.
 func inClusterConfig() (*Config, error) {
-	const (
-		tokenFile  = "/var/run/secrets/kubernetes.io/serviceaccount/token"
-		rootCAFile = "/var/run/secrets/kubernetes.io/serviceaccount/ca.crt"
-	)
-	host, port := os.Getenv("KUBERNETES_SERVICE_HOST"), os.Getenv("KUBERNETES_SERVICE_PORT")
+	host, port := os.Getenv(EnvVarKubernetesServiceHost), os.Getenv(EnvVarKubernetesServicePort)
 	if len(host) == 0 || len(port) == 0 {
 		return nil, ErrNotInCluster
 	}
@@ -42,7 +45,7 @@ func inClusterConfig() (*Config, error) {
 		return nil, err
 	}
 	return &Config{
-		Host:            "https://" + net.JoinHostPort(host, port),
+		Host:            scheme + net.JoinHostPort(host, port),
 		CACertPool:      pool,
 		BearerToken:     string(token),
 		BearerTokenFile: tokenFile, // TODO should I re-check this periodically? Or lazily on a bad response?
